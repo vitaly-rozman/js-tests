60609476b3060001e73ece1a15760b1ef8dff511
Vitaly Rozman <vitaliy.rozman@huffingtonpost.com>
2016-02-04 22:54:01 -0500

    Last two tests

--- app/recursion.js
+++ app/recursion.js
@@ -2,7 +2,8 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.recursionAnswers = {
   listFiles: function(data, dirName) {
-    var files = new Array();
+    var files = [];
+
     dirName = (dirName === undefined) || (dirName === data.dir) ? undefined : dirName;
     data.files.forEach(function(element) {
         if ( typeof element === 'object' ) {
@@ -16,7 +17,22 @@ exports.recursionAnswers = {
   },
 
   permute: function(arr) {
+    var result = [];
 
+    if ( arr.length == 1 ) {
+        result.push(arr);
+    } else {
+        var first = arr.shift();
+        this.permute(arr).forEach(function(element) {
+            for (var index=0; index <= element.length; ++index) {
+                var copy = element.slice();
+                copy.splice(index, 0, first);
+                result.push(copy);
+            }
+        });
+    }
+
+    return result;
   },
 
   fibonacci: function(n) {
@@ -28,6 +44,22 @@ exports.recursionAnswers = {
   },
 
   validParentheses: function(n) {
+    var result = [];
+    var braces = '()';
 
+    if ( n == 1) {
+        return [braces];
+    } else {
+        this.validParentheses(n-1).forEach(function(element) {
+            for (var index=0; index < element.length; ++index) {
+                var option = element.substr(0, index) + braces + element.substr(index);
+                if ( result.indexOf(option) == -1 ) {
+                    result.push(option);
+                }
+            }
+        });
+    }
+
+    return result;
   }
 };
